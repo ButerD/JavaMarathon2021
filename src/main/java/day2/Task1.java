4c562d6d43446deebedd01e154ee5610025b0d91
ButerD <buter81@gmail.com>
2021-01-21 19:11:28 +0300

    Add Day2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,18 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
 
+        if( num <= 0 )
+            System.out.println("Ошибка ввода");
+        else{
+            if( num <= 4 ) System.out.println("Малоэтажный дом");
+            if( (num >= 5) && (num <= 8) ) System.out.println("Среднеэтажный дом");
+            if( num >= 9 ) System.out.println("Многоэтажный дом");
+        }
     }
 }
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,15 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
-
+        Scanner scaner = new Scanner(System.in);
+        Double x = scaner.nextDouble();
+        Double y = 0.0;
+        if( (x > -3) && (x < 5))    y = (x + 3.0)*(x*x - 2.0);
+        if(x >= 5)                  y = (x*x - 10.0)/(x + 7.0);
+        if(x < -3)                  y = 420.0;
+        System.out.println(y);
     }
 }
