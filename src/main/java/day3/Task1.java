78f7af6da289019f3b5591378f11ec71f7b2c318
ButerD <buter81@gmail.com>
2021-01-23 13:18:07 +0300

    Add day3

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,48 @@
 package day3;
+/*
+Реализовать, используя следующие данные:
+Москва, Владивосток, Ростов - Россия
+Рим, Милан, Турин - Италия
+Ливерпуль, Манчестер, Лондон - Англия
+Берлин, Мюнхен, Кёльн - Германия
+
+ */
+import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean flag = true;
+        while(flag) {
+            switch (scanner.nextLine()) {
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                case "stop" :
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+                    break;
 
+            }
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Double num, div;
 
+        for (int i = 0; i < 5; i++) {
+            num = scanner.nextDouble();
+            div = scanner.nextDouble();
+            if (div == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+                System.out.println(num / div);
+        }
     }
 }
